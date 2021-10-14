bd898c8ec67fd19b7bc12b01b8f778e0e3455565
Shane-Michael Thompson <shane2michael@gmail.com>
2021-10-13 21:08:15 -0500

    added items for navbar
    added items

---
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.keepclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
