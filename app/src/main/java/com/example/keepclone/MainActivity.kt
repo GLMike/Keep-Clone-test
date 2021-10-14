90f14bd25b80be4e5638f1751c7d5f864cad5b02
sadlerb <sadlerb22@gmail.com>
2021-10-14 12:39:21 -0500

    Got it Up and running

--- app/src/main/java/com/example/keepclone/MainActivity.kt
+++ app/src/main/java/com/example/keepclone/MainActivity.kt
@@ -2,13 +2,36 @@ package com.example.keepclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val drawerLayout:DrawerLayout = findViewById(R.id.drawer_layout)
+        val nav_view:NavigationView = findViewById(R.id.nav_view)
+        val menu_toggle:ImageButton = findViewById(R.id.menu_toggle)
+
+        // Button to open nav
+        menu_toggle.setOnClickListener{
+            drawerLayout.openDrawer(Gravity.LEFT)
+        }
+
+        nav_view.setNavigationItemSelectedListener(this)
 
 
+            }
+    // Get Selected Item
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(applicationContext,"${item.title.toString()} was selected",Toast.LENGTH_SHORT).show()
+        return true
     }
+
 }
+
+
